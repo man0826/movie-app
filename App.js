88abedb1e9ad2e07cae8ffabf02726c5c085610e
man0826 <man08269055@icloud.com>
2020-11-02 23:25:58 +0900

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { StatusBar } from "react-native";
+import {
+  Provider as PaperProvider,
+  DarkTheme as DarkThemePaper,
+  DefaultTheme as DefaultThemePaper,
+} from "react-native-paper";
+import {
+  NavigationContainer,
+  DarkTheme as DarkThemeNavigation,
+  DefaultTheme as DefaultThemeNavigation,
+} from "@react-navigation/native";
+import { Provider as StoreProvider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import AnimatedSplash from "react-native-animated-splash-screen";
 
-export default function App() {
+import BottomNavigation from "./src/navigation/BottomNavigation";
+import store, { persistor } from "./src/store/";
+
+const App = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 4000);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StoreProvider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <AnimatedSplash
+          translucent={true}
+          isLoaded={isLoaded}
+          logoImage={require("./assets/logo.gif")}
+          backgroundColor={"#1a1d28"}
+          logoHeight={500}
+          logoWidht={500}
+        >
+          <AppInner />
+        </AnimatedSplash>
+      </PersistGate>
+    </StoreProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const AppInner = () => {
+  const theme = useSelector((state) => state.theme.theme);
+
+  DefaultThemePaper.colors.primary = "#1ae1f2";
+  DarkThemePaper.colors.primary = "#1ae1f2";
+  DarkThemePaper.colors.accent = "#1ae1f2";
+
+  DarkThemeNavigation.colors.background = "#192734";
+  DarkThemeNavigation.colors.card = "#000";
+
+  return (
+    <PaperProvider
+      theme={theme === "dark" ? DarkThemePaper : DefaultThemePaper}
+    >
+      <StatusBar
+        barStyle={theme === "dark" ? "light-content" : "dark-content"}
+      />
+      <NavigationContainer
+        theme={theme === "dark" ? DarkThemeNavigation : DefaultThemeNavigation}
+      >
+        <BottomNavigation />
+      </NavigationContainer>
+    </PaperProvider>
+  );
+};
+
+export default App;
